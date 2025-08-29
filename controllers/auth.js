ea6f9dc66d3415a3775cfbcea7eaefac9f5f423d
naseem1999 <naseemmuhammad700@gmail.com>
2025-08-29 17:15:29 +0500

    Create a user in data base and also hash password using bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,13 @@ const asyncHandler = require("../middleware/async");
 //access public
 
 exports.register=asyncHandler(async(req,res,next)=>{
+    const {name,email,password,role}=req.body;
+    const user=await User.create({
+        name,
+        email,
+        password,
+        role
+    })
     res.status(200).json({
         success:true
     })
