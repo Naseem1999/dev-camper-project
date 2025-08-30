5dfa2d3b5c6265998e0db44be80814fe0ee0452d
naseem1999 <naseemmuhammad700@gmail.com>
2025-08-30 17:43:26 +0500

    make a protected middileware for routes

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -4,13 +4,14 @@ const courseRouter=require('./courses')
 const {getBootCamp,getBootCamps,createBootCamp,updateBootCamp,deleteBootCamp,getBootCampInRadius,bootcampPhotoUpload} =require('../controllers/bootcamps')
 const advancedResults=require('../middleware/advanceResult');
 const BootCamp=require('../models/BootCamp')
+const {protect}=require('../middleware/auth')
 const router=express.Router()
 //re-route into other resource route
 router.use('/:bootcampid/courses',courseRouter)
 
 
 router.route('/radius/:zipcode/:distance').get(getBootCampInRadius)
-router.route('/').get(advancedResults(BootCamp,'courses'),getBootCamps).post(createBootCamp);
-router.route('/:id').get(getBootCamp).put(updateBootCamp).delete(deleteBootCamp)
-router.route('/:id/photo').put(bootcampPhotoUpload)
+router.route('/').get(advancedResults(BootCamp,'courses'),getBootCamps).post(protect,createBootCamp);
+router.route('/:id').get(getBootCamp).put(protect,updateBootCamp).delete(protect,deleteBootCamp)
+router.route('/:id/photo').put(protect,bootcampPhotoUpload)
 module.exports=router
--- routes/courses.js
+++ routes/courses.js
@@ -3,12 +3,12 @@ const {getCourses,getSingleCourse,addCourse,updateCourse,deleteCourse} =require(
 const advancedResults=require('../middleware/advanceResult');
 const Course=require('../models/Course')
 const router=express.Router({mergeParams:true})
-
+const {protect}=require('../middleware/auth')
 
 router.route('/').get(advancedResults(Course,{
     path:'bootcamp',
     select:'name description'
-}),getCourses).post(addCourse)
-router.route('/:id').get(getSingleCourse).put(updateCourse).delete(deleteCourse)
+}),getCourses).post(protect,addCourse)
+router.route('/:id').get(getSingleCourse).put(protect,updateCourse).delete(protect,deleteCourse)
 
 module.exports=router
